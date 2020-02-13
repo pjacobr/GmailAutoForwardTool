1c346aa0555b20fd3c3a16304523e4c72a982c07
pjacobr <jacobp1794@gmail.com>
2020-02-13 06:27:23 -0700

    automation module completed

--- googleAutomation.py
+++ googleAutomation.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
 URL = 'https://accounts.google.com'  # URL for gmail SignIn
 path_to_chromedriver = '/usr/bin/chromedriver'  #  path to chromedriver in ubuntu
@@ -21,6 +22,9 @@ def wait_and_send_keys(browser, path, message):
         EC.element_to_be_clickable((By.XPATH, path))).send_keys(message)
 
 def get_browser():
+    """
+    function for getting browser
+    """
     try:
         options = webdriver.ChromeOptions()
         # options.add_argument('headless')
@@ -37,18 +41,23 @@ def google_login(browser, mail_address, mail_password):
     """
     try:
         sId = browser.session_id  # assign browser session id to a variable
-        browser.get(URL)
-        browser.implicitly_wait(3)
-        wait_and_send_keys(browser, '//*[@id="identifierId"]', mail_address)
-        wait_and_click(browser, '//*[@id="identifierNext"]')
-        wait_and_send_keys(browser, '//*[@id="password"]/div[1]/div/div/input', mail_password)
-        wait_and_click(browser, '//*[@id="passwordNext"]')
+        browser.get(URL) # get browser
+        browser.implicitly_wait(3) # wait for google signIn page loading for three seconds
+        wait_and_send_keys(browser, '//*[@id="identifierId"]', mail_address) # auto type email address using selenium
+        wait_and_click(browser, '//*[@id="identifierNext"]') # click "Next" button
+        wait_and_send_keys(browser, '//*[@id="password"]/div[1]/div/div/input', mail_password) # type email password
+        wait_and_click(browser, '//*[@id="passwordNext"]') # click "Next" button
+        time.sleep(5) # wait for five seconds while the gmail page is loading
+        return browser.session_id # return browser session id
     except Exception as ex:
         print("Exception in google login: {0}".format(ex))
 
 def close_browser(browser):
+    """
+    function for closing the browser
+    """
     try:
-        browser.close()
+        browser.close() # close browser
     except Exception as ex:
         print("Exception in close browser function: {0}".format(ex))
         browser.close()
